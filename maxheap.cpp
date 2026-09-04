1718bba1e5e85a093212334fa348ce2067098e6b
Siva-Kumar16 <sivakumarvella16@gmail.com>
2026-09-04 17:14:51 +0530

    Add files via upload

---
+++ maxheap.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildMaxHeap(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+}
+
+int main() {
+    vector<int> arr = {4, 10, 3, 5, 1};
+
+    buildMaxHeap(arr);
+
+    for (int x : arr)
+        cout << x << " ";
+}
